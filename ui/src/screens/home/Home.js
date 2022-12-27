902250ae21047f4106323457022092a02dd12765
TechStudent11 <76978184+TechStudent10@users.noreply.github.com>
2022-12-26 17:06:04 -0700

    about to convert to ts

--- ui/src/screens/home/Home.js
+++ ui/src/screens/home/Home.js
@@ -1,32 +1,53 @@
 import React, { useState } from 'react'
 import "./Home.scss"
 
-const SidebarItem = (props) => {
-    return <div className="item">
-        <div className="icon">{props.icon}</div>
-        <p>{props.children}</p>
-    </div>
-}
-
-function Sidebar(props) {
-    return <div className='sidebar'>
-        <SidebarItem icon={<img src="./assets/user.png" />}>
-            <p>
-                Sign In
-            </p>
-        </SidebarItem>
-        <SidebarItem icon={<img src="./assets/user.png" />}>
-            <p>
-                Home
-            </p>
-        </SidebarItem>
-    </div>
-}
+import Sidebar, { SidebarItem, getAsset } from "./Sidebar"
+import Separator from '../../components/Separator'
+import Dashboard from './dashboard'
 
 export default function Home(props) {
+    const [currentPage, setPage] = useState(0)
+
+    const pages = [
+        Dashboard
+    ]
+
+    let Component = pages[currentPage]
+
     return (
         <div className={`${props.className} home`}>
-            <Sidebar />
+            <Sidebar>
+                <SidebarItem icon={<img src={getAsset("user.png")} />} rounded>
+                    <p>
+                        Sign In
+                    </p>
+                </SidebarItem>
+                <Separator />
+                <SidebarItem
+                    icon={<img src={getAsset("dashboard.png")}/>}
+                    onClick={() => setPage(0)}
+                >
+                    <p>
+                        Dashboard
+                    </p>
+                </SidebarItem>
+                <SidebarItem icon={<img src={getAsset("folder.png")}/>}>
+                    <p>
+                        Projects
+                    </p>
+                </SidebarItem>
+                <SidebarItem icon={<img src={getAsset("cogwheel.png")}/>} style={{
+                    position: "absolute",
+                    bottom: "0"
+                }}>
+                    <p>
+                        Settings
+                    </p>
+                </SidebarItem>
+            </Sidebar>
+            <div className='page'>
+                <Component />
+            </div>
         </div>
     )
 }
---
+++ ui/src/screens/home/dashboard/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./dashboard.scss"
+
+export default function Dashboard(props) {
+    return <div className="dashboard">
+        Hello dashboard!
+    </div>
+}
