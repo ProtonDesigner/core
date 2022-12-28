64f774ffaac2d476dcfbfa80cf0aeda9201ee626
TechStudent11 <76978184+TechStudent10@users.noreply.github.com>
2022-12-28 14:12:08 -0700

    revert electron to JS

---
+++ main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { autoUpdater } = require("electron-updater");
+const { default: installExtension, REACT_DEVELOPER_TOOLS } = require("electron-devtools-installer")
+
+app.on("ready", () => {
+	autoUpdater.checkForUpdatesAndNotify();
+});
+
+let win
+
+function createWindow () {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+        preload: path.join(__dirname, 'preload.ts')
+        }
+    })
+
+    win.loadFile(path.join(__dirname, 'ui', 'index.html'))
+
+    if (!app.isPackaged) {
+        win.webContents.openDevTools()
+    }
+}
+
+app.whenReady().then(() => {
+    installExtension(REACT_DEVELOPER_TOOLS)
+        .then((ex_name) => console.log(`Added "${ex_name}" Extension`))
+        .catch((err) => console.log("An error occured when attempting to install extensions:", err))
+
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
